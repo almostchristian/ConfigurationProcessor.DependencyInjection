ed1dc7c2cec908e039c80c2db16db512a36f048a
almostchristian <almostchristian@live.com>
2023-09-26 16:55:32 +0800

    Add ConfigurationPath property

--- sample/TestWebApiGenerator/ServiceRegistrationExtensions.cs
+++ sample/TestWebApiGenerator/ServiceRegistrationExtensions.cs
@@ -5,7 +5,7 @@ namespace TestWebApiGenerator;
 
 internal static partial class ServiceRegistrationExtensions
 {
-    [GenerateConfiguration("Services", ExcludedSections = new[] { "Hsts" }, ImplicitSuffixes = new[] { "Instrumentation", "Exporter" })]
+    [GenerateConfiguration("Services", ExcludedSections = new[] { "Hsts" }, ImplicitSuffixes = new[] { "Instrumentation", "Exporter" }, ConfigurationPath = nameof(WebApplicationBuilder.Configuration))]
     public static partial void AddServicesFromConfiguration(this WebApplicationBuilder builder);
 
     // [GenerateServiceRegistration("Services")]
